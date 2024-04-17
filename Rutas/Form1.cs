cc6214ed728750cc937cc9cc71f2c7a6c0c13360
Jefferson <201613681i@ueta.edu.pe>
2024-04-17 10:40:27 -0500

    se agrega funcionalidad a los botones profesor

--- Rutas/Form1.cs
+++ Rutas/Form1.cs
@@ -24,12 +24,48 @@ namespace Rutas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                using (var bd = new RutasEntities())
+                {
+                    var Cliente = new Clientes();
+
+                    Cliente.ClienteID = Convert.ToInt32(textBox1.Text);
+                    Cliente.Nombre = textBox2.Text;
+                    Cliente.TipoCliente = textBox3.Text;
+                    bd.Clientes.Add(Cliente);
+                    bd.SaveChanges();
+                    MessageBox.Show(" registrado correctamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al registrar cliente: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                using (var bd = new RutasEntities())
+                {
+                    var ventas = new Ventas();
+                    ventas.VentaID = Convert.ToInt32(textBox4.Text);
+                    ventas.ClienteID = Convert.ToInt32(textBox1.Text);
+                    ventas.Ruta = textBox5.Text;
+                    ventas.Precio = Convert.ToInt32(textBox6.Text);
+                    ventas.NumeroPersonas = Convert.ToInt32(textBox7.Text);
+                    bd.Ventas.Add(ventas);
+                    bd.SaveChanges();
+                    MessageBox.Show(" registrado correctamente.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al registrar cliente: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
