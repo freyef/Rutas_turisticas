44cdad6f6b28f4c8ff0d7673983aac6b5224ebee
Jefferson <201613681i@ueta.edu.pe>
2024-04-17 11:32:58 -0500

    profesor  tuve  muchos inconvenientes para este examen

--- Rutas/Form1.Designer.cs
+++ Rutas/Form1.Designer.cs
@@ -45,6 +45,16 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -100,7 +110,7 @@
             this.label5.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label5.Location = new System.Drawing.Point(315, 87);
+            this.label5.Location = new System.Drawing.Point(315, 109);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(61, 13);
             this.label5.TabIndex = 4;
@@ -112,7 +122,7 @@
             this.label6.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label6.Location = new System.Drawing.Point(315, 112);
+            this.label6.Location = new System.Drawing.Point(315, 132);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(38, 13);
             this.label6.TabIndex = 5;
@@ -124,7 +134,7 @@
             this.label7.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label7.Location = new System.Drawing.Point(315, 139);
+            this.label7.Location = new System.Drawing.Point(315, 155);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(47, 13);
             this.label7.TabIndex = 6;
@@ -136,7 +146,7 @@
             this.label8.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label8.Location = new System.Drawing.Point(315, 166);
+            this.label8.Location = new System.Drawing.Point(315, 179);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(110, 13);
             this.label8.TabIndex = 7;
@@ -173,7 +183,7 @@
             // 
             this.textBox4.BackColor = System.Drawing.SystemColors.HotTrack;
             this.textBox4.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox4.Location = new System.Drawing.Point(382, 80);
+            this.textBox4.Location = new System.Drawing.Point(388, 102);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 20);
             this.textBox4.TabIndex = 11;
@@ -182,7 +192,7 @@
             // 
             this.textBox5.BackColor = System.Drawing.SystemColors.HotTrack;
             this.textBox5.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox5.Location = new System.Drawing.Point(382, 105);
+            this.textBox5.Location = new System.Drawing.Point(388, 128);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(100, 20);
             this.textBox5.TabIndex = 12;
@@ -191,7 +201,7 @@
             // 
             this.textBox6.BackColor = System.Drawing.SystemColors.HotTrack;
             this.textBox6.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox6.Location = new System.Drawing.Point(382, 131);
+            this.textBox6.Location = new System.Drawing.Point(388, 152);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 13;
@@ -200,7 +210,7 @@
             // 
             this.textBox7.BackColor = System.Drawing.SystemColors.HotTrack;
             this.textBox7.ForeColor = System.Drawing.SystemColors.Menu;
-            this.textBox7.Location = new System.Drawing.Point(431, 159);
+            this.textBox7.Location = new System.Drawing.Point(431, 172);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(100, 20);
             this.textBox7.TabIndex = 14;
@@ -227,11 +237,127 @@
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label9.Location = new System.Drawing.Point(315, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "ID Cliente:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox8.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox8.Location = new System.Drawing.Point(388, 76);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(169, 283);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(270, 31);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "VER DESCUENTO ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label11.Location = new System.Drawing.Point(65, 329);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(139, 13);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "INGRESE ID CLIENTE:";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox9.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox9.Location = new System.Drawing.Point(210, 322);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 21;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label12.Location = new System.Drawing.Point(157, 387);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Precio:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label13.Location = new System.Drawing.Point(94, 358);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(110, 13);
+            this.label13.TabIndex = 23;
+            this.label13.Text = "Numero Personas:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox10.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox10.Location = new System.Drawing.Point(210, 351);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 24;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox11.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox11.Location = new System.Drawing.Point(210, 380);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 25;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.button3.Location = new System.Drawing.Point(278, 419);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "DESCUENTO";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(606, 497);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox7);
@@ -275,6 +401,16 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Rutas/Form1.cs
+++ Rutas/Form1.cs
@@ -50,22 +50,119 @@ namespace Rutas
             {
                 using (var bd = new RutasEntities())
                 {
-                    var ventas = new Ventas();
-                    ventas.VentaID = Convert.ToInt32(textBox4.Text);
-                    ventas.ClienteID = Convert.ToInt32(textBox1.Text);
+                    var ventas = new Ventas();                  
+                    int ventaID;
+                    if (int.TryParse(textBox4.Text, out ventaID))
+                    {
+                        ventas.VentaID = ventaID;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El ID de venta ingresado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    int clienteID;
+                    if (int.TryParse(textBox8.Text, out clienteID))
+                    {
+                        ventas.ClienteID = clienteID;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El ID de cliente ingresado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
                     ventas.Ruta = textBox5.Text;
-                    ventas.Precio = Convert.ToInt32(textBox6.Text);
-                    ventas.NumeroPersonas = Convert.ToInt32(textBox7.Text);
+
+                    decimal precio;
+                    if (decimal.TryParse(textBox6.Text, out precio))
+                    {
+                        ventas.Precio = precio;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El precio ingresado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    int numeroPersonas;
+                    if (int.TryParse(textBox7.Text, out numeroPersonas))
+                    {
+                        ventas.NumeroPersonas = numeroPersonas;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El número de personas ingresado no es válido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
                     bd.Ventas.Add(ventas);
                     bd.SaveChanges();
-                    MessageBox.Show(" registrado correctamente.");
+                    MessageBox.Show("Registrado correctamente.");
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error al registrar cliente: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Error al registrar venta: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int clienteID = Convert.ToInt32(textBox9.Text); // Obtener el ID del cliente desde algún campo en tu formulario
+                using (var bd = new RutasEntities())
+                {
+                    // Buscar el cliente en la base de datos
+                    var cliente = bd.Clientes.FirstOrDefault(c => c.ClienteID == clienteID);
+                    if (cliente == null)
+                    {
+                        MessageBox.Show("El cliente no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    // Obtener la cantidad de personas registradas para este cliente
+                    var cantidadPersonas = bd.Ventas.Where(v => v.ClienteID == clienteID).Sum(v => v.NumeroPersonas);
+
+                    // Obtener el precio inicial para este cliente
+                    var precioInicial = bd.Ventas.Where(v => v.ClienteID == clienteID).Sum(v => v.Precio * v.NumeroPersonas);
+
+                    // Calcular el porcentaje de descuento según la cantidad de personas registradas
+                    decimal descuento = 0;
+                    if (cantidadPersonas >= 2 && cantidadPersonas < 7)
+                    {
+                        descuento = 0.08m; // 8%
+                    }
+                    else if (cantidadPersonas >= 7 && cantidadPersonas < 16)
+                    {
+                        descuento = 0.13m; // 13%
+                    }
+                    else if (cantidadPersonas >= 16)
+                    {
+                        descuento = 0.15m; // 15%
+                    }
+
+                    // Calcular el precio final con descuento
+                    decimal  precioFinal =   descuento;
+                    decimal precio = descuento * precioFinal;
+                    decimal final = precio - precioFinal;
+
+                    // Mostrar los resultados
+                    MessageBox.Show($"Cliente: {cliente.Nombre}\nCantidad de personas: {cantidadPersonas}\nPrecio inicial: {precioInicial}\nPorcentaje de descuento: {descuento * 100}%\nPrecio final con descuento: {precioFinal}", "Descuento", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al calcular descuento: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
